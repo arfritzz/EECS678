5e7320f6bcbde49cd797b836ea6310dbc6aee883
anna <arfritz97@yahoo.com>
2020-04-01 22:13:00 -0500

    updating

--- project1/main.c
+++ project1/main.c
@@ -234,6 +234,9 @@ void parse_Input(char* command){
 		PIPE
 	-----------*/
 	else if (strstr(command, "|")) {
+		if (index(command, "|") != rindex(command, "|")) {
+			
+		}
 		redirect('|', command);
 	}
 
@@ -268,13 +271,20 @@ void parse_Input(char* command){
 			
 				char* newPath = strstr(command, "/");
 
-				if((setenv("PATH",newPath,0))< 0){
-					printf("path set error\n");
+				if (newPath == NULL) {
+					printf("Incorrect path name. Try again\n\n");
+					exit(0);
 				}
-				else {
-					relPath = getenv("PATH");
-					printf("new PATH variable: %s\n", relPath);
+				else{
+					if((setenv("PATH",newPath,0))< 0){
+						printf("path set error\n");
+					}
+					else {
+						relPath = getenv("PATH");
+						printf("new PATH variable: %s\n", relPath);
+					}
 				}
+
 			}
 
 			// Invalid path syntax
